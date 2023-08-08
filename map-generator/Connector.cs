5f2d780b8faf51a73b340a953ef832798e6ff880
Finlay Metcalfe <metcalfinl@ecs.vuw.ac.nz>
2023-08-08 19:49:21 +1200

    Feature|MapMaker: Created basic implementation of connector class

--- map-generator/Connector.cs
+++ map-generator/Connector.cs
@@ -2,5 +2,32 @@ namespace map_generator;
 
 public class Connector
 {
+    private static readonly Lazy<Connector> empty = new Lazy<Connector>(
+        () => new Connector(-1, -1)
+        );
+
+    public static Connector Empty
+    {
+        get { return empty.Value; }
+    }
+
+    private int x;
+    private int y;
+
+    public Connector(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
 
 }
