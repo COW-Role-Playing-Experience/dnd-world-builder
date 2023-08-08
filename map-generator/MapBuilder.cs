3b101485d89fe50178cd803ef438e6a2a1cddfdb
Finlay Metcalfe <metcalfinl@ecs.vuw.ac.nz>
2023-08-08 20:03:39 +1200

    Feature|MapMaker: Implemented basic seeded RNG, generation code now creates two connected rooms

--- map-generator/MapBuilder.cs
+++ map-generator/MapBuilder.cs
@@ -1,13 +1,17 @@
+using map_generator;
+
 public class MapBuilder
 {
     private RoomTile[,] tiles;
     private int xSize;
     private int ySize;
+    private Random rng;
 
-    public MapBuilder(int xSize, int ySize)
+    public MapBuilder(int xSize, int ySize, Random rng)
     {
         this.xSize = xSize;
         this.ySize = ySize;
+        this.rng = rng;
         this.tiles = new RoomTile[xSize, ySize];
         this.emptyTilesMap();
         System.Console.WriteLine("testing");
@@ -28,16 +32,21 @@ public class MapBuilder
 
     private void initRoom()
     {
+        // TEST GENERATION
         int initX = this.xSize / 2;
         int initY = this.ySize / 2;
-        RoomBuilder room = new RoomBuilder(initX, initY, 5, 5, this.tiles);
+        RoomBuilder room = new RoomBuilder(initX, initY, 5, 5, this.tiles, this.rng);
+        room.bakeRoomTiles().generateConn(Direction.NORTH);
+        Connector conn = room.getConnector(Direction.NORTH);
+        RoomBuilder room2 = new RoomBuilder(conn.getX(), conn.getY() - 1, 2, 2, this.tiles, this.rng);
+        room2.bakeRoomTiles();
     }
 
     public void printMap()
     {
-        for (int x = 0; x < xSize; x++)
+        for (int y = 0; y < ySize; y++)
         {
-            for (int y = 0; y < ySize; y++)
+            for (int x = 0; x < xSize; x++)
             {
                 RoomTile tile = this.tiles[x, y];
                 System.Console.Write(tile.getChar());
