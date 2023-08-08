7bebed97a5a707336ff27adcf46bfdd6e96c5495
Finlay Metcalfe <metcalfinl@ecs.vuw.ac.nz>
2023-08-08 19:58:45 +1200

    Feature|MapMaker: Basic connector generation, grid edit code now in builder pattern method

--- map-generator/RoomBuilder.cs
+++ map-generator/RoomBuilder.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using map_generator;
 
 public class RoomBuilder
 {
@@ -7,21 +9,47 @@ public class RoomBuilder
     private int x;
     private int y;
     private readonly RoomTile[,] gridTiles;
-
+    private Random rng;
     private bool[] connSides;
+    private Connector[] connectors;
 
-    public RoomBuilder(int x, int y, int xSize, int ySize, RoomTile[,] gridTiles)
+    public RoomBuilder(int x, int y, int xSize, int ySize, RoomTile[,] gridTiles, Random rng)
     {
         this.xSize = xSize;
         this.ySize = ySize;
         this.x = x;
         this.y = y;
         this.gridTiles = gridTiles;
-
-        this.initRoomTiles();
+        this.rng = rng;
+        this.connectors = new Connector[4];
+        Array.Fill(this.connectors, Connector.Empty);
+        this.connSides = new bool[4];
     }
 
-    private void initRoomTiles()
+
+
+    public bool hasConn(Direction direction)
+    {
+        if (direction != Direction.NONE)
+        {
+            return this.connSides[(int)direction];
+        }
+        throw new ArgumentException("Direction value does not correspond " +
+                                    "with a valid side of this room", "direction");
+    }
+
+    public Connector getConnector(Direction direction)
+    {
+        if (direction != Direction.NONE)
+        {
+            return this.connectors[(int)direction];
+        }
+        throw new ArgumentException("Direction value does not correspond " +
+                                    "with a valid side of this room", "direction");
+    }
+
+    // Builder Methods
+    public RoomBuilder bakeRoomTiles()
     {
         for (int i = x; i < x + this.xSize; i++)
         {
@@ -31,6 +59,47 @@ public class RoomBuilder
                 tile.setEmpty(false);
             }
         }
+
+        return this;
+    }
+
+    public RoomBuilder generateConn(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.NORTH:
+                if (!connSides[0])
+                {
+                    int xCoord = this.x + rng.Next(this.xSize);
+                    this.connectors[0] = new Connector(xCoord, y - 1);
+                }
+                break;
+            case Direction.EAST:
+                if (!connSides[1])
+                {
+                    int yCoord = this.y + rng.Next(this.ySize);
+                    this.connectors[1] = new Connector(x + this.xSize + 1, yCoord);
+                }
+                break;
+            case Direction.SOUTH:
+                if (!connSides[2])
+                {
+                    int xCoord = this.x + rng.Next(this.xSize);
+                    this.connectors[2] = new Connector(xCoord, y + this.ySize + 1);
+                }
+                break;
+            case Direction.WEST:
+                if (!connSides[3])
+                {
+                    int yCoord = this.y + rng.Next(this.ySize);
+                    this.connectors[3] = new Connector(x - 1, yCoord);
+                }
+                break;
+            default:
+                throw new ArgumentException("Direction value does not correspond " +
+                                            "with a valid side of this room", "direction");
+        }
+        return this;
     }
 
 }
