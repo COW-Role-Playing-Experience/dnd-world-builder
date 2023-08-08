c54eb83c890a5bb85ee36c0b47b21f2bd61bf224
Finlay Metcalfe <metcalfinl@ecs.vuw.ac.nz>
2023-08-08 20:03:39 +1200

    Feature|MapMaker: Implemented basic seeded RNG, generation code now creates two connected rooms

--- map-generator/Program.cs
+++ map-generator/Program.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main()
     {
+        Random rng = new Random(16);
         Console.WriteLine("Hello, World!");
-        MapBuilder map = new MapBuilder(25, 25);
+        MapBuilder map = new MapBuilder(15, 15, rng);
         map.printMap();
     }
 }
