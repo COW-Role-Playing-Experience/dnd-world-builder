40760b0b00b5d70f6d37e4fe262c00ce0c795f70
Finlay Metcalfe <metcalfinl@ecs.vuw.ac.nz>
2023-08-14 13:22:07 +1200

    Fix|MapMaker: Repassed skipped files for pre-commit

--- map-generator/RoomTheme.cs
+++ map-generator/RoomTheme.cs
@@ -1,42 +1,49 @@
 public class RoomTheme
 {
-  private int id;
-  private int minSize;
-  private int maxSize;
-  private int[] connectorIds;
-  private int maxConnectors;
-  private int minConnectors;
+    private int id;
+    private int minSize;
+    private int maxSize;
+    private int[] connectorIds;
+    private int maxConnectors;
+    private int minConnectors;
 
-  public RoomTheme(int id, int minSize, int maxSize, int minConnectors, int maxConnectors, int[] connectorIds){
-    this.id = id;
-    this.minSize = minSize;
-    this.maxSize = maxSize;
-    this.minConnectors = minConnectors;
-    this.maxConnectors = maxConnectors;
-    this.connectorIds = connectorIds;
-  }
+    public RoomTheme(int id, int minSize, int maxSize, int minConnectors, int maxConnectors, int[] connectorIds)
+    {
+        this.id = id;
+        this.minSize = minSize;
+        this.maxSize = maxSize;
+        this.minConnectors = minConnectors;
+        this.maxConnectors = maxConnectors;
+        this.connectorIds = connectorIds;
+    }
 
-  public int getId(){
-    return this.id;
-  }
+    public int getId()
+    {
+        return this.id;
+    }
 
-  public int getMinSize(){
-    return this.minSize;
-  }
+    public int getMinSize()
+    {
+        return this.minSize;
+    }
 
-  public int getMaxSize(){
-    return this.maxSize;
-  }
+    public int getMaxSize()
+    {
+        return this.maxSize;
+    }
 
-  public int getMinConnector(){
-    return this.minConnectors;
-  }
-  
-  public int getMaxConnector(){
-    return this.maxConnectors;
-  }
-  
-  public int[] getConnectorIds(){
-    return this.connectorIds;
-  }
+    public int getMinConnector()
+    {
+        return this.minConnectors;
+    }
+
+    public int getMaxConnector()
+    {
+        return this.maxConnectors;
+    }
+
+    public int[] getConnectorIds()
+    {
+        return this.connectorIds;
+    }
 }
