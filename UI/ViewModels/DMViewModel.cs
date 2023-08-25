efdba87d612e616277b016c0d7693121bd118ca9
Nathan Bridge-Earney <bridgenath1@ecs.vuw.ac.nz>
2023-08-25 18:23:54 +1200

    Feature|UIUX: Added Button to Hide UI
    Added button to DM view that hides all UI elements except the button itself but rather changes the opacity to make it less obtrusive.

--- UI/ViewModels/DMViewModel.cs
+++ UI/ViewModels/DMViewModel.cs
@@ -1,6 +1,39 @@
+using System;
+using System.Reactive;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ReactiveUI;
+
 namespace UI.ViewModels;
 
 public class DMViewModel : ViewModelBase
 {
+    private bool isUIVisible = true;
+    private double uiButtonOpacity = 1.0;
 
+    public bool IsUIVisible
+    {
+        get => isUIVisible;
+        set => this.RaiseAndSetIfChanged(ref isUIVisible, value);
+    }
+
+    public double UIButtonOpacity
+    {
+        get => uiButtonOpacity;
+        set => this.RaiseAndSetIfChanged(ref uiButtonOpacity, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> ToggleUIVisibility { get; }
+
+    public DMViewModel()
+    {
+        ToggleUIVisibility = ReactiveCommand.Create(ToggleButton);
+    }
+
+    private void ToggleButton()
+    {
+        IsUIVisible ^= true;
+
+        UIButtonOpacity = IsUIVisible ? 1.0 : 0.5;
+    }
 }
