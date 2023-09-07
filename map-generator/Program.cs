c6bebfc8a68f6caffdbc9be60f3386c7b6fd3551
Anfri <haywarmatt@vuw.ac.nz>
2023-09-07 12:31:25 +1200

    Refactor|MapMaker: Add ImageSharp nuget dependency, create ImageSharp demo

--- map-generator/Program.cs
+++ map-generator/Program.cs
@@ -7,9 +7,11 @@ class Program
 {
     static void Main()
     {
-        Random rng = new Random();
-        Console.WriteLine("Hello, World!");
-        MapBuilder map = new MapBuilder(200, 40, rng, 0.8);
-        map.setTheme("data/dungeon-theme/").initRoom().fillGaps().printMap();
+        ImageSharpTest.ImageTest.Demo();
+
+        // Random rng = new Random();
+        // Console.WriteLine("Hello, World!");
+        // MapBuilder map = new MapBuilder(200, 40, rng, 0.8);
+        // map.setTheme("data/dungeon-theme/").initRoom().fillGaps().printMap();
     }
 }
