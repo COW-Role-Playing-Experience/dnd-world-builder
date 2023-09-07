89283fb9ae3e9ef54a985c8bf356856e8f92e62d
Anfri <haywarmatt@vuw.ac.nz>
2023-09-07 12:36:07 +1200

    Refactor|MapMaker: Reset Program.cs file to call to mapgenerator functions

--- map-generator/Program.cs
+++ map-generator/Program.cs
@@ -7,11 +7,11 @@ class Program
 {
     static void Main()
     {
-        ImageSharpTest.ImageTest.Demo();
+        //ImageSharpTest.ImageTest.Demo();
 
-        // Random rng = new Random();
-        // Console.WriteLine("Hello, World!");
-        // MapBuilder map = new MapBuilder(200, 40, rng, 0.8);
-        // map.setTheme("data/dungeon-theme/").initRoom().fillGaps().printMap();
+        Random rng = new Random();
+        Console.WriteLine("Hello, World!");
+        MapBuilder map = new MapBuilder(200, 40, rng, 0.8);
+        map.setTheme("data/dungeon-theme/").initRoom().fillGaps().printMap();
     }
 }
