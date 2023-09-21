79869786344374e742f022e9c89ee181b366d320
David Lindsay <lindsadavi2@ecs.vuw.ac.nz>
2023-09-21 20:05:02 +1200

    Merge|UIUX: Create new seed generator, that generates a seed usable for map generation

--- UI/ViewModels/MapGeneratorViewModel.cs
+++ UI/ViewModels/MapGeneratorViewModel.cs
@@ -1,8 +1,35 @@
+
+using System;
+using Avalonia.Controls;
+using System.Windows.Input;
+using System.Linq;
+
 namespace UI.ViewModels;
 
 
 public class MapGeneratorViewModel : ViewModelBase
 {
+    private int MapSeed = new Random().Next(1, 999999999);
+    public void GenerateSeed(TextBox SeedTextBox)
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(1, 999999999);
+        SeedTextBox.Text = randomNumber.ToString();
+    }
 
-
+    public void SeedBoxWritten(object sender, TextChangedEventArgs e)
+    {
+        TextBox textBox = (TextBox)sender;
+        string text = textBox.Text;
+        if (!int.TryParse(text, out int result))
+        {
+            int caretIndex = textBox.CaretIndex;
+            textBox.Text = string.Concat(text.Where(char.IsDigit));
+            textBox.CaretIndex = Math.Max(0, caretIndex - 1);
+        }
+        else
+        {
+            MapSeed = result;
+        }
+    }
 }
--- UI/Views/MapGeneratorView.axaml.cs
+++ UI/Views/MapGeneratorView.axaml.cs
@@ -1,6 +1,10 @@
-using Avalonia;
+
+
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using UI.ViewModels;
+using System.Windows.Input;
 
 namespace UI.Views;
 
@@ -9,10 +13,22 @@ public partial class MapGeneratorView : UserControl
     public MapGeneratorView()
     {
         InitializeComponent();
+        DataContext = new MapGeneratorViewModel();
     }
 
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private void GenerateSeed(object sender, RoutedEventArgs e)
+    {
+        TextBox SeedTextBox = this.FindControl<TextBox>("SeedTextBox");
+        (DataContext as MapGeneratorViewModel)?.GenerateSeed(SeedTextBox);
+    }
+
+    private void SeedBoxWritten(object sender, TextChangedEventArgs e)
+    {
+        (DataContext as MapGeneratorViewModel)?.SeedBoxWritten(sender, e);
     }
 }
