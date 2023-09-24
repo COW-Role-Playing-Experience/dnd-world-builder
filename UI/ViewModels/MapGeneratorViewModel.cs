be98f3faf5a9b6584a3799115a55a94c0c9dd5cd
David Lindsay <davidtlindsay@gmail.com>
2023-09-25 11:22:59 +1300

    Merge|UIUX: Dynamically load theme categories

--- UI/ViewModels/MapGeneratorViewModel.cs
+++ UI/ViewModels/MapGeneratorViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using Avalonia.Controls;
 using System.Windows.Input;
 using System.Linq;
+using System.IO;
 
 namespace UI.ViewModels;
 
@@ -32,4 +33,28 @@ public class MapGeneratorViewModel : ViewModelBase
             MapSeed = result;
         }
     }
+
+    //Dynamically add Themes to the map generator view, based on what folders exist in Assets\Data
+    public void makeThemeBoxes(ComboBox themesBox)
+    {
+
+        //Get array of subdirectories
+        string directoryPrefix = Directory.GetCurrentDirectory().Split(new[] { "UI" }, StringSplitOptions.None)[0];
+        string dataDirectory = Path.Combine(directoryPrefix, "Assets", "data");
+        if (!Directory.Exists(dataDirectory)) return;
+        string[] subdirectories = Directory.GetDirectories(dataDirectory);
+        //Loop through each subdirectory
+        foreach (string subdirectory in subdirectories)
+        {
+            if (subdirectory.Contains("-theme"))
+            {
+                //Process and add theme name to ComboBox
+                string themeName = subdirectory.Split(new[] { "Assets" + Path.DirectorySeparatorChar + "data" + Path.DirectorySeparatorChar }, StringSplitOptions.None)[1];
+                themeName = themeName.Split(new[] { "-" }, StringSplitOptions.None)[0];
+                string themeCapitalized = char.ToUpper(themeName[0]) + themeName.Substring(1);
+                themesBox.Items.Add(themeCapitalized);
+            }
+        }
+    }
+
 }
